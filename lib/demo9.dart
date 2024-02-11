e003c13cd8a71c69b4c75a462887e3d849d05473
Turing158 <15818961209@163.com>
2024-02-11 21:26:40 +0800

    demo9网络布局

--- lib/demo9.dart
+++ lib/demo9.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
@@ -6,16 +8,16 @@ void main(){
         title:const Text("demo9"),
         backgroundColor: Colors.lime,
       ),
-      body: const GridTest(),
+      body: const GridTest3(),
     ),
   ));
 }
 
-class GridTest extends StatelessWidget{
-  const GridTest({super.key});
+
+class GridTest1 extends StatelessWidget{
+  const GridTest1({super.key});
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return GridView.count(
       scrollDirection: Axis.horizontal,
       crossAxisCount: 3,
@@ -31,6 +33,59 @@ class GridTest extends StatelessWidget{
         Icon(Icons.handshake_outlined,color: Colors.lightGreen,),
         Icon(Icons.image_rounded,color: Colors.deepOrange,),
       ],
+    );
+  }
+}
+
+class GridTest2 extends StatelessWidget{
+  const GridTest2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.extent(
+        maxCrossAxisExtent: 100,
+      childAspectRatio: 1.0,
+      children: const <Widget>[
+        Icon(Icons.account_balance_wallet,color: Colors.green,),
+        Icon(Icons.balance,color: Colors.brown,),
+        Icon(Icons.camera,color: Colors.lightBlueAccent,),
+        Icon(Icons.data_object,color: Colors.orange,),
+        Icon(Icons.egg,color: Colors.cyan,),
+        Icon(Icons.facebook_outlined,color: Colors.greenAccent,),
+        Icon(Icons.gamepad,color: Colors.deepPurple,),
+        Icon(Icons.handshake_outlined,color: Colors.lightGreen,),
+        Icon(Icons.image_rounded,color: Colors.deepOrange,),
+      ],
+    );
+  }
+}
+
+class GridTest3 extends StatelessWidget{
+  const GridTest3({super.key});
+
+  List<Widget> getList(){
+    List<Widget> list = [];
+    for(int i = 0;i<100;i++){
+      list.add(Container(
+        color: Color.fromRGBO(Random().nextInt(255), Random().nextInt(255), Random().nextInt(255), 1),
+        alignment: Alignment.center,
+        child: Text(
+            "${i+1}",
+          style: TextStyle(
+            color: Color.fromRGBO(Random().nextInt(255), Random().nextInt(255), Random().nextInt(255), 1)
+          ),
+        ),
+      ));
+    }
+    return list;
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 5,
+      children: getList(),
     );
   }
 
